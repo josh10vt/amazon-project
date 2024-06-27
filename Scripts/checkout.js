ca379e3eba254828914d74e17ca0fef09a020862
joshokoro <josh10@vt.edu>
2024-06-26 22:44:43 -0400

    Save cart in localStorage

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -36,7 +36,7 @@ cart.forEach((cartItem) => {
           </div>
           <div class="product-quantity">
             <span>
-              Quantity: <span class="quantity-label">${cart.quantity}</span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
               Update
